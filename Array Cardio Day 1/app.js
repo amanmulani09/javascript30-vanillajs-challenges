a93ae61738f4a88b3947179508369d566a8163d4
Aman Mulani <mulaniman0504@gmail.com>
2022-12-29 21:18:32 +0530

    added new problems

--- Array Cardio Day 1/app.js
+++ Array Cardio Day 1/app.js
@@ -65,6 +65,7 @@ const people = ['Aman, Mulani','Rahul, Kothawale',"Nilesh, Age","Vishal,Kothawal
 
 //Array methods to work on ?
 
+ 
 
 /// 1. Array.prototype.filter()
 //Q Filter the list of inventors for those who were born in the 1500's
@@ -74,13 +75,48 @@ const people = ['Aman, Mulani','Rahul, Kothawale',"Nilesh, Age","Vishal,Kothawal
 let inventorBornin15 = [];
 const fiterdList = inventors.map(user => user.year.toString().includes('15') ? inventorBornin15.push(user): user );;
 
-console.log(inventorBornin15)
-console.log(fiterdList);
+// console.log(inventorBornin15)
+// console.log(fiterdList);
 
 //Video Solution ???
+const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year <1600)
+console.table('*** filter console ',fifteen);
+
+
+//Array.prototype.map()
+
+//2. give us an array of the inventory first and last names
+
+//My solution 
+// const fullNamesOfInventors = inventors.map(inventor=> `${inventor.first} ${inventor.last}`)
+
+// console.log(fullNamesOfInventors)
+
+
+//Video solution ??
+
+const fullNames = inventors .map(inventor => inventor.first + ' ' + inventor.last)
+console.log('****** map ',fullNames)
+//almost same husshhh
 
 
 
+//Array.prototype.sort();
 
+//sort the inventors by birthdate,older to youngst
+  //aweesomeee
+// let investorsSortedAsBirthDate = inventors.sort((firstPerson,secondPerson)=>{
+//     if(firstPerson.year>secondPerson.year){
+// return 1
+//     } else{
+//         return -1;
+//     }
+// });
 
+// console.table(investorsSortedAsBirthDate)
+
+//oneLiner
+
+let sortedByBirth = inventors.sort((first,second)=> first.year > second.year ? 1 : -1);
+console.log('******** sort',sortedByBirth)
 
